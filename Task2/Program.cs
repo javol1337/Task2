ea59ebbc3e019cc1f7e8d6e497a77433175a1a00
marius <marius.bubulas@gmail.com>
2020-04-23 20:13:05 +0300

    Add calculator logic

---
+++ Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world, this is an addition calculator.");
+            Console.WriteLine("Please enter the first number");
+
+            var firstNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter the second number");
+
+            var secondNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Sum is: {firstNumber + secondNumber}");
+
+        }
+    }
+}
